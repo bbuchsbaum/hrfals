8704f830a9eeaf4e4360785b6c2a231f52605c5a
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-05-27 16:56:53 -0400

    Add CG solver integration for spatial CF-ALS

--- R/utils_spatial.R
+++ R/utils_spatial.R
@@ -71,3 +71,44 @@ build_voxel_laplacian <- function(volume, connectivity = 6) {
   list(L = L, degree = as.numeric(degree))
 }
 
+#' Construct Spatial h-update System Matrix
+#'
+#' Helper used by the spatial CF-ALS implementation. Forms the sparse
+#' block matrix \eqn{A_H = BlockDiag(LHS_v) + \lambda_s (L \otimes I_d)}.
+#'
+#' @param lhs_block_list list of length `v` with `d x d` matrices for each voxel.
+#' @param lambda_s spatial regularization strength.
+#' @param L_mat sparse Laplacian matrix (`v x v`).
+#' @param d number of basis functions.
+#' @param v number of voxels.
+#' @return `dgCMatrix` representing `A_H`.
+#' @keywords internal
+construct_A_H_sparse <- function(lhs_block_list, lambda_s, L_mat, d, v) {
+  A_block <- Matrix::bdiag(lhs_block_list)
+  if (lambda_s > 0) {
+    kron_part <- lambda_s * Matrix::kronecker(L_mat, Matrix::Diagonal(d))
+    A_block <- A_block + kron_part
+  }
+  A_block
+}
+
+#' Construct Block-Jacobi Preconditioner
+#'
+#' Forms a block-diagonal preconditioner matrix for the conjugate gradient solver.
+#'
+#' @param lhs_block_list list of per-voxel `d x d` matrices.
+#' @param lambda_s spatial regularization strength.
+#' @param degree_vec numeric degree vector from the Laplacian.
+#' @param d number of basis functions.
+#' @param v number of voxels.
+#' @return block-diagonal `dgCMatrix` preconditioner.
+#' @keywords internal
+construct_preconditioner <- function(lhs_block_list, lambda_s, degree_vec, d, v) {
+  pre_list <- vector("list", v)
+  for (vx in seq_len(v)) {
+    pre_list[[vx]] <- solve(lhs_block_list[[vx]] +
+                             lambda_s * degree_vec[vx] * diag(d))
+  }
+  Matrix::bdiag(pre_list)
+}
+
